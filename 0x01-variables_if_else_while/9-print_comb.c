c996aecf567be4baeaba8f7704fca1022c8dde8f
EmezieNgene <ngenechiemezie@gmail.com>
2022-03-14 18:33:41 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,16 @@
  */
 int main(void)
 {
-	int n =48;
-	int com = 44;
+	int n;
 
-	while (n <= 57)
+	for (n = 48; n = 58; n++)
 	{
 		putchar(n);
 		if (n != 57)
 		{
-			putchar(com);
-			putchar(32);
+			putchar(',');
+			putchar(' ');
 		}
-		n += 1;
 	}
 	putchar('\n')
 	return (0);
