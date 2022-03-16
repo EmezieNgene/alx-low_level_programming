28a595d6a2731fbd2d2915715c3ffd9eafee20b5
EmezieNgene <ngenechiemezie@gmail.com>
2022-03-16 09:46:02 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar to the screen and moves to a new line 
+ *Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My functions and nexted loops
